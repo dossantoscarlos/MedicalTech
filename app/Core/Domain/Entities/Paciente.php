4a3ff7a2f87de70417af0e14fe5c4e3e08c256a5
Carlos Eduardo <anjoazrael10@gmail.com>
2025-10-17 00:22:12 -0300

    corrigindo indentacao com pint. removendo regra de quebra de linha

--- app/Core/Domain/Entities/Paciente.php
+++ app/Core/Domain/Entities/Paciente.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace App\Core\Domain\Entities;
@@ -21,11 +22,11 @@ class Paciente extends Pessoa
         public readonly DateTime $data_nascimento,
     ) {
         parent::__construct(
-          nome: $nome,
-          sobrenome: $sobrenome,
-          cpf: $cpf,
-          rg: $rg,
-          endereco: $endereco
+            nome: $nome,
+            sobrenome: $sobrenome,
+            cpf: $cpf,
+            rg: $rg,
+            endereco: $endereco
         );
     }
 
